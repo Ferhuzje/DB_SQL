8a624a409d821728308e8e0abc003bb199be1724
Ferhuzje <null>
2021-07-08 08:31:48 +0000

    04 Import CSV

---
+++ 04_SQL_DataImport/04_db_csv_import_SQL.sql
@@ -0,0 +1,32 @@
+/* USER DB Vers. 1*/
+
+/* DB */
+DROP DATABASE IF EXISTS boo;
+CREATE DATABASE IF NOT EXISTS boo;
+USE boo;
+
+
+/* Tabelle users*/
+
+DROP TABLE IF EXISTS cats;
+
+CREATE TABLE IF NOT EXISTS cats
+(
+    id INT NOT NULL PRIMARY KEY,
+    cat_name VARCHAR(20) NOT NULL,
+    fur_color VARCHAR(20) NOT NULL,
+    age INT NOT NULL
+);
+
+#DESCRIBE cats;
+
+/* CSV-Datei parsen */
+LOAD DATA LOCAL INFILE "04_SQL_DataImport/cats_export.csv"
+INTO TABLE cats
+FIELDS TERMINATED BY ";"
+LINES TERMINATED by "\n"
+IGNORE 1 ROWS;
+;
+
+SELECT * FROM cats;
+
